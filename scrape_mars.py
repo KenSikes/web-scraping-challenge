a1abc8690e1cec75db9c295d938c7790215bb442
Ken <kensikes988@gmail.com>
2021-05-01 18:44:35 -0700

    saving

--- scrape_mars.py
+++ scrape_mars.py
@@ -7,3 +7,36 @@ import requests
 def init_browser(): 
     executable_path = {'executable_path': ChromeDriverManager().install()}
     return Browser('chrome', headless=True, **exec_path)
+
+# Create Mission to Mars global dictionary that can be imported into Mongo
+mars_info = {}
+
+# NASA Mars News
+def scrape_mars_news():
+
+        # Initialize browser 
+        browser = init_browser()
+
+        #browser.is_element_present_by_css("div.content_title", wait_time=1)
+
+        #visit Nasa URL 
+        url = 'https://redplanetscience.com/'
+        browser.visit(url)
+
+        # HTML Object
+        html = browser.html
+
+        # Parse HTML with Beautiful Soup
+        soup = bs(html, 'html.parser')
+
+        # Retrieve the latest element that contains news title and news_paragraph
+        news_title = (soup.find_all('div', class_='content_title'))[0].get_text()
+        news_p = (soup.find_all('div', class_='article_teaser_body'))[0].get_text()
+
+        # Dictionary entry from MARS NEWS
+        mars_info['news_title'] = news_title
+        mars_info['news_paragraph'] = news_p
+
+        return mars_info
+
+        browser.quit()
